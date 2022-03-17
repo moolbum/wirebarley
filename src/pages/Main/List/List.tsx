2485aa0d0dfc3fa589f72da0e17cefda44c1630f
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-03-17 21:50:02 +0900

    Add : 텍스트 변수처리

--- src/pages/Main/List/List.tsx
+++ src/pages/Main/List/List.tsx
@@ -2,33 +2,40 @@ import React from 'react';
 import * as S from './List.style';
 import Remittance from './Remittance';
 import { useSelect } from '../../../hooks/useSelect';
+import { remittanceData, recipientData } from './data';
+
 const REMITTANCE_COUNTRY = 'remittanceCountry';
 const RECIPIENT_COUNTRY = 'recipientCountry';
+const KOREAN_EXCHANGERATE = 'KRW';
+const USA_EXCHANGERATE = 'USD';
 
 const List = () => {
   const [selectCountry, select] = useSelect();
-  console.log(select.remittanceCountry);
   return (
     <S.List>
       <S.ListTitle>
         송금국가:
         <S.Select name={REMITTANCE_COUNTRY} onChange={selectCountry}>
-          <S.Option>미국 (USD)</S.Option>
-          <S.Option>호주 (AUD)</S.Option>
+          {remittanceData.map(data => {
+            return <S.Option key={data.id}>{data.country}</S.Option>;
+          })}
         </S.Select>
       </S.ListTitle>
+
       <S.ListTitle>
         수취국가:
         <S.Select name={RECIPIENT_COUNTRY} onChange={selectCountry}>
-          <S.Option>한국 (KRW)</S.Option>
-          <S.Option>일본 (JPY)</S.Option>
-          <S.Option>필리핀 (PHP)</S.Option>
+          {recipientData.map(data => {
+            return <S.Option key={data.id}>{data.country}</S.Option>;
+          })}
         </S.Select>
       </S.ListTitle>
+
       <S.ListTitle>
-        환율: {select.recipientCountry || 'KRW'}/
-        {select.remittanceCountry || 'USD'}
+        환율: {select.recipientCountry || KOREAN_EXCHANGERATE}/
+        {select.remittanceCountry || USA_EXCHANGERATE}
       </S.ListTitle>
+
       <S.ListTitle>
         송금액: <Remittance remittanc={select.remittanceCountry} />
       </S.ListTitle>
